7649894f939b4d12e80f8fb299a48b7fa950a662
Felix Christl <fchristl@hipsquare.net>
2018-06-15 14:08:23 +0200

    added option to enable/disable drag image

--- projects/ngx-easily-draggable/src/lib/ngx-easily-draggable.directive.ts
+++ projects/ngx-easily-draggable/src/lib/ngx-easily-draggable.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostListener} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
 import {NgxEasilyDraggableService} from "./ngx-easily-draggable.service";
 
 @Directive({
@@ -6,12 +6,19 @@ import {NgxEasilyDraggableService} from "./ngx-easily-draggable.service";
 })
 export class NgxEasilyDraggableDirective {
 
+  @Input() enableDragImage = true;
+
   constructor(private elementRef: ElementRef,
               private service: NgxEasilyDraggableService) {
     this.elementRef.nativeElement.draggable = 'true';
   }
 
-  @HostListener('dragstart') private dragStart() {
+  @HostListener('dragstart', ['$event']) private dragStart(event: DragEvent) {
     this.service.draggedElement = this.elementRef;
+    if (!this.enableDragImage) {
+      const dragImage = new Image();
+      dragImage.style.display = 'none';
+      event.dataTransfer.setDragImage(dragImage, 0, 0);
+    }
   }
 }
